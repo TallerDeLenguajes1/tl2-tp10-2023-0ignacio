6703e7c1c3efce8c2380db10fff7c74aa086212c
0ignacio <ignaciogonzaley@gmail.com>
2024-02-26 07:21:13 -0300

    se simplifico la estructura de algunos viewmodels

--- obj/Debug/net7.0/tl2-tp10-2023-0ignacio.AssemblyInfo.cs
+++ obj/Debug/net7.0/tl2-tp10-2023-0ignacio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl2-tp10-2023-0ignacio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bc4824f447f5e4728066a3f20d7aa2cfa4298ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+476b4c8226f7ccdde8a762dba9406fbedc30e6f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl2-tp10-2023-0ignacio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl2-tp10-2023-0ignacio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
